9d1c83e4cfdf954c6e29282f4ac0fa182c145a62
Tyler Piazza <tyler_piazza@college.harvard.edu>
2019-02-23 11:55:47 -0500

    handling 2010 files

--- tyler_stuff/new_capy_2010.py
+++ tyler_stuff/new_capy_2010.py
@@ -1,28 +1,35 @@
-# for downloading city values from 2010
-# Tyler Piazza 12/5
+# for downloading city values from 2000
+# Tyler Piazza 1/24/19
 import numpy as np
 import networkx as nx
 import json
 import csv
 import matplotlib.pyplot as plt
+import os
+import csv
 
-from capy import single_brackets, skew, edge, skew_prime, half_edge, half_edge_infinity, morans_I, dissimilarity, gini
+from capy import single_brackets, skew, edge, skew_prime, half_edge, half_edge_infinity, morans_I, dissimilarity, gini, true_half_edge_infinity
 
-cities = ['Albany-Schenectady-Troy', 'Ann Arbor MI', 'Athens-Clark County GA', 'Austin-Round Rock TX', 'Bloomington IN', 'Boston-Cambridge-Newton MA', 'Boulder CO', 'Bridgeport-Stamford-Norwalk CT', 'Burlington-South Burlington VT', 'Cedar Rapids IA' , 'Chicago-Naperville-Elgin IL-IN-WI', 'Colorado Springs CO', 'Des Moines-West Des Moines IA', 'Duluth MN-WI', 'El Paso TX', 'Flint MI',  'Grand Rapids-Wyoming MI',  'Harrisburg-Carlisle PA', 'Huntingdon PA',  'Iowa city IA', 'Ithaca NY', 'Jacksonville FL', 'Junction City KS', 'Kansas city MO-KS', 'Lafayette-West Lafayette IN', 'Lancaster PA', 'Las Vegas-Henderson-Paradise NV', 'Lincoln NE', 'Los Angeles-Long Beach-Anaheim CA', 'Madison WI',  'McAllen-Edingburg-Mission TX','Miami-Fort Lauderdale-West Palm Beach FL','New Haven-Milford CT', 'New Orleans-Metairie LA', 'New York-Newark-Jersey City NY-NJ-PA',  'Oklahoma City-OK','Orlando-Kissimmee-Sanford FL', 'Philadelphia-Camden-Wilmington PA-NJ-DE-MD', 'Phoenix-Mesa-Scottsdale AZ', 'Pittsburgh PA', 'Plattsburgh NY', 'Providence-Warwick RI-MA', 'Reno NV', 'Rio Grande City TX', 'Riverside-San Bernadino-Ontario CA', 'Rochester NY',  'Salt Lake City UT', 'San Antonio-New Braunfels TX', 'San Diego-Carlsbad CA', 'Santa Cruz-Watsonville CA', 'Santa Fe NM', 'Savannah GA','Syracuse NY', 'Tallahassee FL', 'Tampa-St Petersburg-Clearwater FL', 'Toledo OH', 'Tucson AZ', 'Tuscaloosa AL','Virginia Beach-Norfolk-Newport News VA-NC', 'Youngstown-Warren-Boardman OH-PA']
-# for now, let's do only a few cities
-#cities = ['Albany-Schenectady-Troy', 'Ann Arbor MI', 'Athens-Clark County GA']
+
+CSA_code_dict = {}
+
+# first get a dictionary of CSA codes
+with open('CSA_Codes.csv' , 'rU') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        if row[0] != "Metro Area" and row[0]:
+            code = row[0]
+            code = code[0 : len(code) - 3]
+            CSA_code_dict[row[1]] = code
+
 
 b_scores = [[]]
 h_scores = [[]]
 a_scores = [[]]
 
 
-b_scores.append(["City", "Total polulation", "Percent Black", "Percent White", "Edge" , "HEdge", "HEdgeInfinity", "Dissimilarity", "Gini", "Moran's I"])
+b_scores.append(["City", "Total polulation", "Percent Black", "Percent White", "Edge" , "Edge rank",  "HEdge", "HEdge rank", "HEdgeInfinity (True)", "HEdgeInfinity (True) rank", "Typo HEI", "Typo HEI rank", "Dissimilarity", "Dissimilarity rank",  "Gini", "Gini rank", "Moran's I", "Moran's I rank"])
 
-
-#h_scores.append(["City", "Edge" , "HEdge", "Dissimilarity", "Assortativity", "Moran's I" ])
-
-#a_scores.append(["City", "Edge" , "HEdge", "Dissimilarity", "Assortativity", "Moran's I" ])
 
 bEdge = []
 bHEdge = []
@@ -31,26 +38,19 @@ bFrey = []
 bAssort = []
 bMoran = []
 
-"""
-aEdge = []
-aHEdge = []
-aDiss = []
-aFrey = []
-aAssort = []
-aMoran = []
-
-hEdge = []
-hHEdge = []
-hDiss = []
-hFrey = []
-hAssort = []
-hMoran = []
-"""
 
 #we compute scores
-for city in cities:
+#for city in city_names:
+
+
+for filename in os.listdir("2010_JSONS"):
+    #print filename
+    city = CSA_code_dict[filename]
     print (city)
-    with open('json10/'+city+'_data.json') as f:
+    # this isn't entirely accurate...
+    #with open('2010_JSONS/'+city+'_data.json') as f:
+        #data = json.load(f)
+    with open('2010_JSONS/'+filename) as f:
         data = json.load(f)
     #g = nx.adjacency_graph(data)
     g = nx.readwrite.json_graph.adjacency_graph(data)
@@ -63,6 +63,21 @@ for city in cities:
     black = np.zeros((num_vtds,1))
     asian = np.zeros((num_vtds,1))
     hisp = np.zeros((num_vtds,1))
+
+    """
+    hisp_w = np.zeros((num_vtds,1))
+    hisp_b = np.zeros((num_vtds,1))
+    hisp_a = np.zeros((num_vtds,1))
+    hisp_n = np.zeros((num_vtds,1))
+    hisp_o = np.zeros((num_vtds,1))
+    """
+
+
+
+
+
+
+
     amein = np.zeros((num_vtds,1))
     natpac = np.zeros((num_vtds,1))
     other = np.zeros((num_vtds,1))
@@ -70,30 +85,78 @@ for city in cities:
     nb = np.zeros((num_vtds,1))
     nh = np.zeros((num_vtds,1))
     na = np.zeros((num_vtds,1))
+    poc = np.zeros((num_vtds,1))
 
     #here we add the demographics we want to compute the scores for. Currently we compute the scores for white - demographic and for demographic and its complementary set.
     dems = [black, asian, hisp]
     comps = [nb, na, nh]
 
+    # for 2000, from the codebook (and then the second code is what is in the shapefiles)
+    # I am ignoging 007 and 014 for now
+    # not hispanic
+    #nhgis00012: NOT HISPANIC, WHITE ALONE
+       #nhgis00013: NOT HISPANIC, BLACK ALONE
+       #nhgis00014: NOT HISPANIC, ASIAN ALONE
+       #nhgis00015: NOT HISPANIC, NATIVE HAWAIIAN/PACIFIC ISLANDER ALONE
+       #nhgis00016: NOT HISPANIC, OTHER RACE ALONE
+
+       # and then the HISPANIC portion
+       #nhgis00017: HISPANIC, WHITE
+       #nhgis00018: HISPANIC, BLACK
+       #nhgis00019: HISPANIC, ASIAN
+       #nhgis00020: HISPANIC, NATIVE HAWAIIAN/PACIFIC ISLANDER
+       #nhgis00021: HISPANIC, OTHER RACE
+
     # assign demographic population per geographic unit
     for i in range(g.number_of_nodes()):
-        white[i] = g.nodes[i]['DP0110011']
-        black[i] = g.nodes[i]['DP0110012']
-        asian[i] = g.nodes[i]['DP0110014']
-        hisp[i] = g.nodes[i]['DP0110002']
-        natpac[i] = g.nodes[i]['DP0110015']
-        amein[i] = g.nodes[i]['DP0110013']
-        other[i] = g.nodes[i]['DP0110016']
-        tot[i] = g.nodes[i]['DP0110001']
+        # I assume that each category is discrete (there are no people in multiple categories)
+        white[i] = g.nodes[i]['NH_WHITE']
+        black[i] = g.nodes[i]['NH_BLACK']
+        asian[i] = g.nodes[i]['NH_ASIAN']
+
+        # add up the different variants of hispanic
+        hisp[i] =  float(g.nodes[i]['H_WHITE']) + float(g.nodes[i]['H_BLACK']) + float(g.nodes[i]['H_ASIAN']) + float(g.nodes[i]['H_NAHI']) + float(g.nodes[i]['H_OTH'])
+        """
+        hisp_w[i] = g.nodes[i]['H_WHITE']
+        hisp_b[i] = g.nodes[i]['H_BLACK']
+        hisp_a[i] = g.nodes[i]['H_ASIAN']
+        hisp_n[i] = g.nodes[i]['H_NAHI']
+        hisp_o[i] = g.nodes[i]['H_OTH']
+        """
+
+        natpac[i] = g.nodes[i]['NH_NAHI']
+        other[i] = g.nodes[i]['NH_OTH']
+        # need to modify
+        tot[i] = white[i] + black[i] + asian[i] + hisp[i] + natpac[i] + other[i]
         nb[i] = tot[i] - black[i]
         nh[i] = tot[i] - hisp[i]
         na[i] = tot[i] - asian[i]
+
+        poc[i] = tot[i] - white[i]
+
 
 
     # maybe I can use these vectors, sum them up, etc.
     total_white = white.sum()
     total_black = black.sum()
     total_pop = tot.sum()
+
+    """
+    print "white pop is " + str(total_white)
+    print "black pop is " + str(total_black)
+    print "total pop is " + str(total_pop)
+    print "total hispanic pop is " + str(hisp.sum())
+    print "total natpac pop is " + str(natpac.sum())
+    print "total other pop is " + str(other.sum())
+    print "total h_w is " + str(hisp_w.sum())
+    print "total h_b is " + str(hisp_b.sum())
+    print "total h_a is " + str(hisp_a.sum())
+    print "total h_n is " + str(hisp_n.sum())
+    print "total h_o is " + str(hisp_o.sum())
+    """
+
+
+
 
     white_rho = float(total_white) / float(total_pop) * 100.
     black_rho = float(total_black) / float(total_pop) * 100.
@@ -113,148 +176,56 @@ for city in cities:
     btemplist.append(white_rho)
 
     # now compute interesting scoprs
-    btemplist.append(edge(black, white, A))
-    btemplist.append(half_edge(black, white, A))
-    btemplist.append(half_edge_infinity(black, white, A))
-    btemplist.append(dissimilarity(black, tot))
-    btemplist.append(gini(black, tot))
-    #btemplist.append(asq([black, white], A))
-    btemplist.append(morans_I(black,A))
-
-    #bEdge.append(edge(black, white, A))
-    #bHEdge.append(half_edge(black, white, A))
-    #bDiss.append(dissimilarity(black, tot))
-    #bFrey.append(frey(black, white))
-    #bAssort.append(asq([black, white], A))
-    #bMoran.append(morans_I(black,A))
-
+    btemplist.append(edge(poc, white, A))
+    btemplist.append(half_edge(poc, white, A))
+    # note: this is the real half edge infinity
+    btemplist.append(true_half_edge_infinity(poc, white, A))
+    # note - this is the TYPO half edge infinity
+    btemplist.append(half_edge_infinity(poc, white, A))
+    btemplist.append((dissimilarity(poc, tot)[0]))
+    btemplist.append((gini(poc, tot)[0]))
+    btemplist.append(morans_I(poc, A))
     b_scores.append(btemplist)
 
-    """
+def column(matrix, i, k=0):
+    # have to start it at 2 because of the way we formatted b_scores
+    return [row[i] for row in matrix[k:]]
 
-    htemplist = []
-    htemplist.append(city)
-    htemplist.append(edge(hisp, white, A))
-    htemplist.append(half_edge(hisp, white, A))
-    htemplist.append(dissimilarity(hisp, tot))
-    #htemplist.append(frey(hisp, white))
-    #htemplist.append(gini(hisp, tot))
-    #htemplist.append(asq([hisp, white], A))
-    htemplist.append(morans_I(hisp,A))
+def calculate_rank(vector):
+    # 1 goes to largest element, and then 2 goes to the second largest, etc.
+    a = {}
+    rank = 1
+    rev_sorted_list = sorted(vector)
+    rev_sorted_list.reverse()
+    for num in rev_sorted_list:
+        if num not in a:
+            a[num] = rank
+            rank = rank + 1
+    return[a[i] for i in vector]
 
-    hEdge.append(edge(hisp, white, A))
-    hHEdge.append(half_edge(hisp, white, A))
-    hDiss.append(dissimilarity(hisp, tot))
-    #hFrey.append(frey(hisp, white))
-    #hAssort.append(asq([hisp, white], A))
-    hMoran.append(morans_I(hisp,A))
+# returns vector of ratings
+def rank_column(matrix, i):
+    col = column(matrix, i, k=2)
+    return calculate_rank(col)
 
-    h_scores.append(htemplist)
+# for saving ranks - 1 means largest number, 2 means 2nd largest number, etc.
+rank_matrix = []
 
-    atemplist = []
-    atemplist.append(city)
-    atemplist.append(edge(asian, white, A))
-    atemplist.append(half_edge(asian, white, A))
-    atemplist.append(dissimilarity(asian, tot))
-    #atemplist.append(frey(asian, white))
-    #atemplist.append(gini(asian, tot))
-    #atemplist.append(asq([asian, white], A))
-    atemplist.append(morans_I(asian,A))
+# modify this to 11 (it used to be 10)
+for i in range(4, 11):
+    rank_matrix.append(rank_column(b_scores, i))
 
-    aEdge.append(edge(asian, white, A))
-    aHEdge.append(half_edge(asian, white, A))
-    aDiss.append(dissimilarity(asian, tot))
-    #aFrey.append(frey(asian, white))
-    #aAssort.append(asq([asian, white], A))
-    aMoran.append(morans_I(asian,A))
+# interleaves the scores and the rankings (yes, this should be done programatically)
+for i in range(len(b_scores) - 2):
+    now_row = b_scores[i + 2]
+    rank = column(rank_matrix, i, k=0)
+    b_scores[i + 2] = now_row[0:5] + [rank[0], now_row[5], rank[1], now_row[6], rank[2], now_row[7], rank[3], now_row[8], rank[4], now_row[9], rank[5], now_row[10], rank[6]]
 
-    a_scores.append(atemplist)
-    """
 
-# I commented the below out because newline was invalid in Python 2.7
 
-"""
-with open('city scores BW_1105.csv', 'w', newline='') as csvfile:
+# sort by city names
+#b_scores.sort(key=lambda x: x[0])
+
+with open('POC_W_capy_scores_2010.csv', 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=",")
     writer.writerows(b_scores)
-
-with open('city scores HW_1105.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=",")
-    writer.writerows(h_scores)
-
-with open('city scores AW_1105.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=",")
-    writer.writerows(a_scores)
-"""
-
-with open('city scores BW_1105 with pop and rho.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile, delimiter=",")
-    writer.writerows(b_scores)
-
-"""
-with open('city scores HW_1105.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile, delimiter=",")
-    writer.writerows(h_scores)
-
-with open('city scores AW_1105.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile, delimiter=",")
-    writer.writerows(a_scores)
-"""
-
-"""
-#These are the rhos for the cities (in order) I couldn't integrate this into the script, if you find a way that'd be great
-
-percb = [6.3,10.8, 18.5,6.8, 2.4, 6.2,0.7, 8.9,1.4, 2.8,16.8,5.2,4.2,1.3, 2.5,17.2, 5.4,7.7, 2.5,3.2, 2.7,21.2,11.5, 11.8, 2.5, 3.1, 9.7,2.8, 6.7, 3.6,0.3, 19.6,10.3, 32.3,15.6,9.5, 14.2,19.4,4.5,7.7,2.8,4.1, 2.1, 0.04, 6.9,9.9, 1.3,5.9,4.7, 0.85, 0.62,33.1,6.7,31.9,11,11.7,3.1,34.3,30.7,8.8]
-
-perca = [2.8, 7.3,2.5, 4.5, 3.2, 6.2, 3.8, 4.3, 1.8, 1.3, 5.5, 2.4, 2.7, 0.7, 0.9, 0.8, 1.9, 2.4, 0.8, 4, 5.9, 3.2, 2.2, 2.1, 3.8, 1.6, 8.2, 2.9, 3.2, 14.4, 0.9, 2.2, 3.2, 2.4, 9.5, 2.6, 3.7, 4.7, 3.2, 1.6, 0.9, 2.5, 4.5, 0.2, 6.2, 2.3, 3, 1.9, 10.5, 4.2,1.2,1.9, 2.1,2, 2.8, 1.3, 2.4, 0.8, 3.3, 0.5]
-
-perch = [3.9, 3.7, 6.9, 30.9, 2.2, 8.5, 12.3, 16.3, 1.57, 2.2, 20.3, 14.5, 6.1, 1.3, 82.1, 2.9, 7.8, 4.3, 1.3, 5.5, 3.4, 6.8, 9.2, 7.9, 6, 7.4, 28.8, 5.8, 4.9, 44.2, 90.6, 41.4, 13.5, 7.2, 22.3, 10.8, 24.4, 7.8, 29.2, 1.2, 2.2, 9.5, 21.3, 95.6, 46.3, 5.6, 16.2, 53.4, 32, 32.4, 48, 5.4, 3.1, 5.6, 16.1, 5.7, 34.6, 2.3, 5.2, 2.3]
-
-
-fig, ax = plt.subplots()
-ax.scatter(percb, bEdge, c='r', marker='.', label='Black - White')
-ax.scatter(perch, hEdge, c= 'b', marker='.', label='Hispanic - White')
-ax.scatter(perca, aEdge, c='g', marker='.', label='Asian - White')
-
-ax.set_ylabel('Edge Capy Scores')
-ax.set_xlabel('Percentage Minority Population')
-ax.set_title('Edge Capy scores and minority percentages in 60 US cities')
-legend = ax.legend(loc='left', fontsize='small')
-
-plt.savefig('Edge', dpi=100)
-
-fig, ax = plt.subplots()
-ax.scatter(percb, bHEdge, c='r', marker='.', label='Black - White')
-ax.scatter(perch, hHEdge, c= 'b', marker='.', label='Hispanic - White')
-ax.scatter(perca, aHEdge, c='g', marker='.', label='Asian - White')
-
-ax.set_ylabel('Half Edge Capy Scores')
-ax.set_xlabel('Percentage Minority Population')
-ax.set_title('Half Edge Capy scores and minority percentages in 60 US cities')
-legend = ax.legend(loc='left', fontsize='small')
-
-plt.savefig('HEdge', dpi=100)
-
-fig, ax = plt.subplots()
-ax.scatter(percb, bDiss, c='r', marker='.', label='Black - White')
-ax.scatter(perch, hDiss, c= 'b', marker='.', label='Hispanic - White')
-ax.scatter(perca, aDiss, c='g', marker='.', label='Asian - White')
-
-ax.set_ylabel('Dissimilarity Scores')
-ax.set_xlabel('Percentage Minority Population')
-ax.set_title('Dissimilarity scores and minority percentages in 60 US cities')
-legend = ax.legend(loc='left', fontsize='small')
-
-plt.savefig('Diss', dpi=100)
-
-fig, ax = plt.subplots()
-
-ax.scatter(percb, bMoran, c='r', marker='.', label='Black - White')
-ax.scatter(perch, hMoran, c= 'b', marker='.', label='Hispanic - White')
-ax.scatter(perca, aMoran, c='g', marker='.', label='Asian - White')
-ax.set_ylabel('Morans I Scores')
-ax.set_xlabel('Percentage Minority Population')
-ax.set_title('Morans i scores and minority percentages in 60 US cities')
-legend = ax.legend(loc='left', fontsize='small')
-plt.savefig('Moran', dpi=100)
-"""
